06297cdaa094dfc5602d3b3ba0da6a6514bc9497
Matthew Nosworthy <mnosworthy007@gmail.com>
2025-01-25 00:47:00 +0000

    added stored procedure user

--- runner.bat
+++ runner.bat
@@ -6,19 +6,16 @@ cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) &&
 call .env.bat
 
 @REM moves to the directory that psql.exe is located 
-cd "C:\Program Files\PostgreSQL\14\bin"
+cd "C:\Program Files\PostgreSQL\17\bin"
 
 @REM var to make code easier to read
 set CONNECT=psql -f
 
 @REM running all of the files containing postgreSQL
-%CONNECT% "C:\\Users\\User\\Documents\\my-website\\database\\drop tables.pgsql" 
-%CONNECT% "C:\\Users\\User\\Documents\\my-website\\database\\user table.pgsql" 
-%CONNECT% "C:\Users\User\Documents\my-website\database\bank_details table.pgsql" 
-%CONNECT% "C:\Users\User\Documents\my-website\database\refresh token table.pgsql" 
-%CONNECT% "C:\Users\User\Documents\my-website\database\product table.pgsql"
-%CONNECT% "C:\Users\User\Documents\my-website\database\image table.pgsql"
-%CONNECT% "C:\Users\User\Documents\my-website\database\review table.pgsql"
-%CONNECT% "C:\Users\User\Documents\my-website\database\order table.pgsql"
-%CONNECT% "C:\Users\User\Documents\my-website\database\inserts.pgsql"
-%CONNECT% "C:\Users\User\Documents\my-website\database\selects.pgsql"
+%CONNECT% "C:\Users\mnosw\Documents\BorderBales\Database\sql\01_UserTable.pgsql"
+%CONNECT% "C:\Users\mnosw\Documents\BorderBales\Database\sql\02_ProductTable.pgsql" 
+%CONNECT% "C:\Users\mnosw\Documents\BorderBales\Database\sql\03_BankDetailsTable.pgsql" 
+%CONNECT% "C:\Users\mnosw\Documents\BorderBales\Database\sql\04_RefreshTokenTable.pgsql" 
+%CONNECT% "C:\Users\mnosw\Documents\BorderBales\Database\sql\05_OrderTable.pgsql"
+%CONNECT% "C:\Users\mnosw\Documents\BorderBales\Database\sql\06_ImageTable.pgsql"
+%CONNECT% "C:\Users\mnosw\Documents\BorderBales\Database\sql\07_ReviewTable.pgsql"
--- sql/04_RefreshTokenTable.pgsql
+++ sql/04_RefreshTokenTable.pgsql
@@ -8,3 +8,22 @@ CREATE TABLE IF NOT EXISTS "refresh_token" (
   CONSTRAINT fk_user_id_refresh_token
     FOREIGN KEY ("id") REFERENCES "user" ("id")
 );
+
+ALTER TABLE "refresh_token" ADD 
+  CONSTRAINT user_id_unique
+    UNIQUE ("id");
+
+CREATE OR REPLACE FUNCTION upsert_refresh_token(user_id INT, token TEXT)
+RETURNS BOOLEAN AS $$
+BEGIN
+  INSERT INTO "refresh_token" ("id", "token")
+  VALUES (user_id, token)
+  ON CONFLICT ("id") DO UPDATE
+  SET "token" = EXCLUDED."token";
+
+  RETURN TRUE;
+EXCEPTION
+  WHEN OTHERS THEN
+    RETURN FALSE;
+END;
+$$ LANGUAGE plpgsql;
